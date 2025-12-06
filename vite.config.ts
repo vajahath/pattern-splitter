06fe722b7455a722649835d50c616b92fb9a6f34
Vajahath <vajuoff.1@gmail.com>
2025-12-06 18:40:21 +0530

    vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,40 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'pattern-splitter',
-      short_name: 'splitter',
-      description: 'This application is a web-based SVG pattern tiling tool that converts a single, oversized vector file into a multi-page, true-to-scale PDF for printing on standard home paper, complete with alignment marks, cut lines, and a scale ruler for accurate assembly',
-      theme_color: '#4338ca',
-    },
+      manifest: {
+        name: "pattern-splitter",
+        short_name: "splitter",
+        description:
+          "This application is a web-based SVG pattern tiling tool that converts a single, oversized vector file into a multi-page, true-to-scale PDF for printing on standard home paper, complete with alignment marks, cut lines, and a scale ruler for accurate assembly",
+        theme_color: "#4338ca",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+    tailwindcss(),
+  ],
+});
